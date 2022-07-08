bc54954b56f20d13db3c35e35f6d42affd506acf
hp4129889 <hp4129889@gmail.com>
2022-07-09 00:17:51 +0800

    test

--- route.go
+++ route.go
@@ -7,16 +7,16 @@ import (
 )
 
 func DefaultPage(c *gin.Context) {
-	c.Redirect(http.StatusMovedPermanently, "login")
+	c.Redirect(http.StatusMovedPermanently, "index.html")
 }
 
 func LoginPage(c *gin.Context) {
-	c.HTML(http.StatusOK, "login.html", nil)
+	c.HTML(http.StatusOK, "index.html", nil)
 }
 
 func ChatPage(c *gin.Context) {
 	nickname := c.Query("nickname")
-	c.HTML(http.StatusOK, "index.html", gin.H{
+	c.HTML(http.StatusOK, "chat.html", gin.H{
 		"nickname": nickname,
 	})
 }
