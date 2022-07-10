15e9b0f2ff7212e981ede3405561a6faed48c226
hp4129889 <hp4129889@gmail.com>
2022-07-11 07:16:54 +0800

    complete

--- auth.go
+++ auth.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -17,6 +18,27 @@ type User struct {
 	Nickname string
 }
 
+func connetDB() *gorm.DB {
+	viper.SetConfigName("mysql")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./config")
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatal("viper error: ", err)
+	}
+	USERNAME := viper.GetString("db.username")
+	PASSWORD := viper.GetString("db.password")
+	NETWORK := viper.GetString("db.network")
+	SERVER := viper.GetString("db.server")
+	PORT := viper.GetInt32("db.port")
+	DATABASE := viper.GetString("db.database")
+	dsn := fmt.Sprintf("%s:%s@%s(%s:%d)/%s", USERNAME, PASSWORD, NETWORK, SERVER, PORT, DATABASE)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("gorm error: ", err)
+	}
+	return db
+}
+
 func LoginAuth(c *gin.Context) {
 	var (
 		username string
@@ -25,7 +47,7 @@ func LoginAuth(c *gin.Context) {
 	if in, isExist := c.GetPostForm("username"); isExist && in != "" {
 		username = in
 	} else {
-		c.HTML(http.StatusBadRequest, "login.html", gin.H{
+		c.HTML(http.StatusBadRequest, "index.html", gin.H{
 			"error": errors.New("username can't be empty."),
 		})
 		return
@@ -33,46 +55,46 @@ func LoginAuth(c *gin.Context) {
 	if in, isExist := c.GetPostForm("password"); isExist && in != "" {
 		password = in
 	} else {
-		c.HTML(http.StatusBadRequest, "login.html", gin.H{
+		c.HTML(http.StatusBadRequest, "index.html", gin.H{
 			"error": errors.New("password can't be empty"),
 		})
 		return
 	}
-	if nickname, err := Auth(username, password); err == nil {
-		c.Redirect(http.StatusMovedPermanently, "/chat?nickname="+nickname)
-
-	} else {
-		c.HTML(http.StatusUnauthorized, "login.html", gin.H{
+	if nickname, err := Auth(username, password); err != nil {
+		c.HTML(http.StatusUnauthorized, "index.html", gin.H{
 			"error": err,
 		})
 		return
-	}
-}
-
-func CheckPassword(db *gorm.DB, user *User, passwordIn string) (string, error) {
-	if passwordIn != user.Password {
-		return "", errors.New("password is not correct")
 	} else {
-		return user.Nickname, nil
+		c.Redirect(http.StatusMovedPermanently, "/chat?nickname="+nickname)
 	}
 }
 
-func FindUser(db *gorm.DB, usernameIn string) (*User, error) {
+func CheckPassword(user *User, passwordIn string) error {
+	if passwordIn != user.Password {
+		return errors.New("password is not correct")
+	}
+	return nil
+}
+
+func FindUser(db *gorm.DB, username string) (*User, error) {
 	user := new(User)
-	user.Username = usernameIn
-	err := db.Where("username = ?", usernameIn).First(&user).Error
+	user.Username = username
+	err := db.Where("username = ?", username).First(&user).Error
+	if err != nil {
+		return nil, errors.New("user not found")
+	}
 	return user, err
 }
 
-func Auth(usernameIn, passwordIn string) (string, error) {
-	dsn := "query:query123@tcp(34.81.16.122:3306)/demo"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal("connect to cloudsql error: ", err)
-	}
-	if user, err := FindUser(db, usernameIn); err != nil {
-		return "", errors.New("user not found")
+func Auth(username, password string) (string, error) {
+	db := connetDB()
+	if user, err := FindUser(db, username); err != nil {
+		return "", err
 	} else {
-		return CheckPassword(db, user, passwordIn)
+		if err := CheckPassword(user, password); err != nil {
+			return "", err
+		}
+		return user.Username, nil
 	}
 }
